dd721ca55820f471d6dc59dd5cd7f68d03fddbb1
jyip01 <64282396+jyip01@users.noreply.github.com>
2020-05-18 10:51:48 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,50 @@
+let curScore = 0;
+let curQuestion = -1;
 
+const questionSet = [
+  { 
+    text: `What is one of the Cs to consider when selecting a ring?`,
+    ans1: `Care`,
+    ans2: `Comparison`,
+    ans3: `Color`,
+    ans4: `Confidence`
+  },
+
+  {
+    text: `What shape is this diamond?`,
+    ans1: `Pear`,
+    ans2: `Square`,
+    ans3: `Oval`,
+    ans4: `Cushion`
+  },
+
+  {
+    text: `What is this setting?`,
+    ans1: `Halo`,
+    ans2: `Bezel`,
+    ans3: `Prong`,
+    ans4: `Solitaire`
+  }, 
+  {
+    text: `What type of color is this band?`,
+    ans1: `Platinum`,
+    ans2: `Rose Gold`,
+    ans3: `Gold`,
+    ans4: `Sterling Silver`
+  }, 
+  {
+    text: `How do you know if your diamond is well cut?`,
+    ans1: `Larger in size`,
+    ans2: `It reflects more light`,
+    ans3: `Dullness`,
+    ans4: `Smaller in size`
+  }
+];
+
+const ANSWERS = [ 
+  `Color`, 
+  `Cushion`, 
+  `Halo`, 
+  `Rose Gold`, 
+  `It reflects more light`,
+];
